c576161cb3fcc26ff5c2705e48e3bb16d361b824
Gwidas <gwidas.hiroshi@icloud.com>
2021-11-28 13:55:24 +0000

    Deleted rows

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -12,7 +12,13 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 try:
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'bob';")
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+
         connection.commit()
 finally:
     connection.close()
